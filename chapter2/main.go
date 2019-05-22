dfb87cf190a39bde07b2be38a2babfad0b895776
edmundcong <eddy9412@gmail.com>
2019-05-22 11:18:50 +1000

    finished chapter 2

--- chapter2/main.go
+++ chapter2/main.go
@@ -1,7 +1,15 @@
 package main
 
 import "fmt"
+import "math"
 
 func main() {
 	fmt.Println("1 + 1 =", 1 + 1)
+	fmt.Println("1.0 + 1.0 =", 1.0 + 1.0)
+	fmt.Println("5 % 3 =", 5 % 3)
+	fmt.Println(len("Hello, World"))
+	fmt.Println("Hello, World"[1])
+	fmt.Println("Hello, " + "World")
+	fmt.Println("Hello \n world")
+	fmt.Println("11111111 = ", math.Exp2(8)-1)
 }
